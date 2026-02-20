bd8c6fae0c81f2a59329a078ce7c4418d349ba5b
Htay Htay Thwe <130959683+htay-htay-thwe@users.noreply.github.com>
2026-02-20 17:45:25 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,17 @@ import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.use(cookieParser());
+
+  app.enableCors({
+    origin: [
+     '*',
+      // 'https://your-frontend-domain.com',
+    ],
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 4000);
 }
 void bootstrap();
