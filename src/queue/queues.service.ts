bd19c86b48bdb166ef254f7832c105f3045dfffd
htayhtaythwe <htayhtaythwe962@gmail.com>
2026-02-23 18:19:20 +0700

    updated token

--- src/queue/queues.service.ts
+++ src/queue/queues.service.ts
@@ -74,7 +74,7 @@ export class QueuesService {
       });
 
       // The new position in the queue (including this customer)
-      const position = waitingAhead + 1;
+      const position = waitingAhead + 2; // +1 for this customer, +1 because position starts at 1
       // Use totalTables as tableCount
       estimatedWaitTime =
         Math.floor((position - 1) / totalTables) * this.AVERAGE_SERVICE_TIME;
